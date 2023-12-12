4bc1211ca52d8c2ced27a22ae632c41b4d3fcf3a
Brainpaingames <121057254+brainpaingames@users.noreply.github.com>
2023-12-13 00:02:42 +0200

    ps script to copy files and push tem to the site.

---
+++ runDevPowershell.ps1
@@ -0,0 +1,15 @@
+# Define the relative paths
+$targetFolder = ".\dev_build"
+$sourceFolder = "..\WebXR-DePanther-VRTK-main\WebXR-DePanther-VRTK-main\builds\webglbuild1"
+
+# Get the current timestamp
+$timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+
+# Clear the contents of the target folder and copy new files
+Remove-Item "$targetFolder\*" -Recurse -Force
+Copy-Item -Path "$sourceFolder\*" -Destination $targetFolder -Recurse -Force
+
+# Stage changes, commit with timestamp, and push
+git add $targetFolder\*
+git commit -m "Auto commit from PowerShell script at $timestamp"
+git push
